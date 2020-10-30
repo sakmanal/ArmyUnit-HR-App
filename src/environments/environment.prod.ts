be17eff1cfda69fcf97eef251b73f54d06459c41
Sakis Manal <manowarlik@gmail.com>
2020-10-30 22:43:15 +0200

    firebase init files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://localhost:4000',
+  weatherApiUrl: 'https://api.openweathermap.org/data/2.5/weather?q=xanthi&appid=698f51c95bbae70b0bcd07e485878af3&units=metric'
 };
