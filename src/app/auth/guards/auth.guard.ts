423129392d768d738ff07b3d40889e61928e3c51
Sakis Manal <manowarlik@gmail.com>
2020-12-26 16:28:01 +0200

    add return typo

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -6,7 +6,7 @@ import { AuthenticationService } from '../services/authentication.service';
 export class AuthGuard implements CanActivate {
   constructor(private router: Router, private authenticationService: AuthenticationService) { }
 
-canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
 
       const currentUser = this.authenticationService.currentUserValue;
       if (currentUser && currentUser.role == 'admin') {
